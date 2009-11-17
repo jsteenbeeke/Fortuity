bda563b7670c2256b71404013fb676069160f4ee
tyc-pros <tyc-pros@5bf6d0c5-e6fc-4c95-bb24-cc19bded424c>
2009-11-17 21:30:46 +0000

    Work in progress
    git-svn-id: https://fortuity.svn.sourceforge.net/svnroot/fortuity/trunk@8 5bf6d0c5-e6fc-4c95-bb24-cc19bded424c

---
+++ fortuity-spring/src/main/java/com/fortuityframework/spring/SpringEventListener.java
@@ -0,0 +1,78 @@
+/*
+ * Copyright 2009 Jeroen Steenbeeke
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at 
+ *
+ * 		http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License. 
+ */
+package com.fortuityframework.spring;
+
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.ApplicationContext;
+
+import com.fortuityframework.core.dispatch.EventContext;
+import com.fortuityframework.core.dispatch.EventException;
+import com.fortuityframework.core.dispatch.EventListener;
+
+/**
+ * Event listener that dispatches events to annotated Spring Bean methods
+ * 
+ * @author Jeroen Steenbeeke
+ */
+class SpringEventListener implements EventListener {
+	private final String beanName;
+	private final Method method;
+	private final ApplicationContext applicationContext;
+
+	private static final Logger log = LoggerFactory
+			.getLogger(SpringEventListener.class);
+
+	/**
+	 * Creates a new Elan Event Listener based on Spring
+	 * 
+	 * @param beanName
+	 *            The name of the bean that responds to this event
+	 * @param method
+	 *            The method to invoke
+	 * @param context
+	 *            The context to get the bean from
+	 */
+	SpringEventListener(String beanName, Method method,
+			ApplicationContext context) {
+		this.beanName = beanName;
+		this.method = method;
+		this.applicationContext = context;
+	}
+
+	/**
+	 * @see com.fortuityframework.core.dispatch.EventListener#dispatchEvent(com.fortuityframework.core.dispatch.EventContext)
+	 */
+	@Override
+	public void dispatchEvent(EventContext context) throws EventException {
+		Object bean = applicationContext.getBean(beanName);
+
+		try {
+			method.invoke(bean, context);
+		} catch (IllegalArgumentException e) {
+			log.error("Could not invoke Spring event bean method", e);
+		} catch (IllegalAccessException e) {
+			log.error("Could not invoke Spring event bean method", e);
+		} catch (InvocationTargetException e) {
+			log.error("Could not invoke Spring event bean method", e);
+		}
+
+	}
+
+}
---
+++ fortuity-spring/src/main/java/com/fortuityframework/spring/SpringEventListenerLocator.java
@@ -0,0 +1,122 @@
+/*
+ * Copyright 2009 Jeroen Steenbeeke
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at 
+ *
+ * 		http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License. 
+ */
+package com.fortuityframework.spring;
+
+import java.lang.reflect.Method;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationEvent;
+import org.springframework.context.ApplicationListener;
+import org.springframework.context.event.ContextStartedEvent;
+
+import com.fortuityframework.core.annotation.ioc.OnFortuityEvent;
+import com.fortuityframework.core.dispatch.EventContext;
+import com.fortuityframework.core.dispatch.EventListener;
+import com.fortuityframework.core.dispatch.EventListenerLocator;
+import com.fortuityframework.core.event.Event;
+
+/**
+ * EventListenerLocator that uses Spring to find responders to various events.
+ * 
+ * {@code <bean id="elanEventLocator" class="net.elanevent.spring.SpringEventListenerLocator"></bean>}
+ * 
+ * This bean is then used as parameter to an
+ * {@link net.elanevent.core.dispatch.EventBroker} bean, which is used as a
+ * property to a persistence interceptor.
+ * 
+ * <code>
+ *  <bean id="elanEventBroker" class="net.elanevent.core.dispatch.broker.FIFOQueueEventBroker">
+ * 		<property name="eventListenerLocator" ref="elanEventLocator" />
+ *  </bean>
+ * </code>
+ * 
+ * @author Jeroen Steenbeeke
+ * 
+ */
+public class SpringEventListenerLocator implements ApplicationListener,
+		EventListenerLocator {
+	private Map<Class<? extends Event>, List<EventListener>> listeners;
+
+	/**
+	 * Create a new SpringEventListenerLocator. 
+	 * 
+	 */
+	public SpringEventListenerLocator() {
+		listeners = new HashMap<Class<? extends Event>, List<EventListener>>();
+	}
+
+	/**
+	 * @see org.springframework.context.ApplicationListener#onApplicationEvent(org.springframework.context.ApplicationEvent)
+	 */
+	@Override
+	public void onApplicationEvent(ApplicationEvent event) {
+		if (event instanceof ContextStartedEvent) {
+			ApplicationContext context = ((ContextStartedEvent) event)
+					.getApplicationContext();
+
+			for (String beanDefinitionName : context.getBeanDefinitionNames()) {
+				Class<?> type = context.getType(beanDefinitionName);
+
+				for (Method m : type.getMethods()) {
+					registerMethodAsListener(context, beanDefinitionName, m);
+				}
+			}
+		}
+
+	}
+
+	private void registerMethodAsListener(ApplicationContext context,
+			String beanDefinitionName, Method m) {
+		OnFortuityEvent eventRef = m.getAnnotation(OnFortuityEvent.class);
+		if (eventRef != null) {
+			Class<?>[] paramTypes = m.getParameterTypes();
+			if (paramTypes.length == 1
+					&& EventContext.class.isAssignableFrom(paramTypes[0])) {
+				for (Class<? extends Event> eventClass : eventRef.value()) {
+					registerListener(eventClass, beanDefinitionName, m, context);
+				}
+			}
+		}
+	}
+
+	private void registerListener(Class<? extends Event> eventClass,
+			String beanName, Method method, ApplicationContext context) {
+		if (!listeners.containsKey(eventClass)) {
+			listeners.put(eventClass, new LinkedList<EventListener>());
+		}
+
+		listeners.get(eventClass).add(
+				new SpringEventListener(beanName, method, context));
+	}
+
+	/**
+	 * @see net.elanevent.core.dispatch.EventListenerLocator#getEventListeners(java.lang.Class)
+	 */
+	@Override
+	public List<EventListener> getEventListeners(
+			Class<? extends Event> eventClass) {
+		if (listeners.containsKey(eventClass)) {
+			return listeners.get(eventClass);
+		}
+
+		return new LinkedList<EventListener>();
+	}
+
+}
